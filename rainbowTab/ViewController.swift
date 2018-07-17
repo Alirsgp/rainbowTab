b77486d7e09c2acea8a7fcab9d8b85cf9928ecf8
Ali Mohammadian <ali.mohammadian@simoncomputing.com>
2018-07-17 11:57:16 -0400

    Initial Commit

---
+++ rainbowTab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rainbowTab
+//
+//  Created by Ali Mohammadian on 7/17/18.
+//  Copyright © 2018 Ali Mohammadian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
